b8b1373637f75c41b2d3c9483a16822d252eec22
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-01-13 12:42:34 +0000

    Refactor InputSanitizer class

--- app/Support/InputSanitizer.php
+++ app/Support/InputSanitizer.php
@@ -4,23 +4,11 @@ declare(strict_types=1);
 
 namespace Syntatis\WP\Option\Support;
 
-use Syntatis\WP\Option\Option;
-
 use function array_key_exists;
 use function is_array;
 
-/** @phpstan-import-type OptionType from Option */
 class InputSanitizer
 {
-	/** @phpstan-var OptionType */
-	private string $type;
-
-	/** @phpstan-param OptionType $type */
-	public function __construct(string $type)
-	{
-		$this->type = $type;
-	}
-
 	/**
 	 * @param mixed $value
 	 * @return array{__syntatis: mixed}
