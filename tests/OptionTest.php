6cda273ba2d6688d1c91ff2f166fbb7a8106a517
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-05-12 01:03:26 +0700

    Refactor OptionTest.php to use initial values in add_option() calls

--- tests/OptionTest.php
+++ tests/OptionTest.php
@@ -35,8 +35,8 @@ class OptionTest extends TestCase
 	// phpcs:ignore PSR1.Methods.CamelCapsMethodName.NotCamelCaps
 	public function tear_down(): void
 	{
-		delete_option($this->optionName);
 		unregister_setting($this->optionGroup, $this->optionName);
+		delete_option($this->optionName);
 
 		parent::tear_down();
 	}
